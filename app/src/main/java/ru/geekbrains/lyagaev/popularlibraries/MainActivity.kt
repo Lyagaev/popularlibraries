40959be1c6de4a65ba5aeebb911af2742971b021
Lyagaev <70940958+Lyagaev@users.noreply.github.com>
2021-05-30 22:34:58 +0500

    1

--- app/src/main/java/ru/geekbrains/lyagaev/popularlibraries/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lyagaev/popularlibraries/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.geekbrains.lyagaev.popularlibraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.geekbrains.lyagaev.popularlibraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val presenter = MainPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.tag.toString().toInt())
+        }
+
+        binding?.btnCounter1?.setOnClickListener(listener)
+        binding?.btnCounter2?.setOnClickListener(listener)
+        binding?.btnCounter3?.setOnClickListener(listener)
     }
+
+    fun setButtonText(index: Int, text: String) {
+        when(index){
+            0 -> binding?.btnCounter1?.text = text
+            1 -> binding?.btnCounter2?.text = text
+            2 -> binding?.btnCounter3?.text = text
+        }
+    }
+
+
 }
+
+
---
+++ app/src/main/java/ru/geekbrains/lyagaev/popularlibraries/MainView.kt
@@ -0,0 +1,5 @@
+package ru.geekbrains.lyagaev.popularlibraries
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
